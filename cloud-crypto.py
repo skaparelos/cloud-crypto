6f44bfc3c5ac0ae0c5146c4e03037a7409e58672
skaparelos <skaparelos@gmail.com>
2018-04-07 13:52:54 +0100

    minor changes

--- cloud-crypto.py
+++ cloud-crypto.py
@@ -27,7 +27,7 @@ def help(prog_name):
 	print("    python " + prog_name + " -d ../out_e -m ../out_e\metadata.txt -o ../out_d")
 
 
-def hash_file(file_path):
+def hash_file_sha1(file_path):
 	
 	# read data in 64kb chunks
 	BUF_SIZE = 65536
@@ -130,11 +130,13 @@ def get_arguments(argv):
 		if argv[0] == "-h":
 			help(prog_name)
 			exit(0)
-			
+		
+		# verbose mode is enabled
 		elif argv[0] == "-v":
 			global g_VERBOSE
 			g_VERBOSE = True
 			
+		# append to metadata
 		elif argv[0] == "-a":
 			opts["-a"] = True
 			
@@ -158,17 +160,15 @@ import json
 def main(myargs):
 	
 	global g_metadata
-	
+
 	fernet_key = None
 	if "-k" in myargs:
-
-		# Load key
-
-		# user password to decrypt encrypted file containing Fernet key
+		## Load Key
+		# get user password to decrypt encrypted file containing the Fernet key
 		pwd = fernet_key_from_user_pass()
 		fernet_key = decrypt_file(encr_file_path=myargs["-k"], key=pwd)
 	else:
-
+		## Create Key
 		# Create a new Fernet key and use the user's password to encrypt it
 		# We could use user's password to directly encrypt data, but it could be a weak password
 
@@ -218,7 +218,7 @@ def main(myargs):
 			if os.path.isfile(f):
 			
 				# calculate the SHA1 hash of the plaintext, i.e. the hash of the unencrypted file
-				sha1_hash = hash_file(f)
+				sha1_hash = hash_file_sha1(f)
 				
 				# get a unique id 
 				uuid = str(create_uuid())
@@ -255,7 +255,7 @@ def main(myargs):
 		# show final stats
 		print("encrypted " + str(encr_files_ctr) + "/" + str(len(files)) + "!")
 		
-		# if metadata has been given, then check all metadata files to see if something has been deleted
+		# if metadata has been given and user is not appending, then check all metadata files to see if something has been deleted
 		# if it has been deleted, delete it
 		if "-m" in myargs and "-a" not in myargs:
 			for f in metadata:
@@ -267,8 +267,7 @@ def main(myargs):
 		# write metadata to file
 		with open('metadata.txt', 'w') as file:
 			file.write(json.dumps(g_metadata, indent=4, sort_keys=True))
-		
-		print("metadata are stored in " + out_folder + 'metadata.txt')
+		print("metadata written in 'metadata.txt'")
 	
 	# else if decrypt
 	elif "-d" in myargs:
@@ -280,8 +279,6 @@ def main(myargs):
 		# load metadata
 		metadata = json.load(open(myargs["-m"]))
 		print("Total files to decrypt: " + str(len(metadata)))
-		# counts how many files have been decrypted so far
-		decr_files_ctr = 0 
 		
 		out_folder = 'out_d/'
 		# if the user has provided an out folder then use that
@@ -289,15 +286,23 @@ def main(myargs):
 			out_folder = myargs["-o"]
 			if not out_folder.endswith("/"):
 				out_folder += "/"
+
+		# counts how many files have been decrypted so far
+		decr_files_ctr = 0 
 		
 		# decrypt every file
 		for f in metadata:
 		
-			# decrypt file
-			write_file(file_name=out_folder + f, data=decrypt_file(myargs["-d"] + "/" + metadata[f]["uuid"], fernet_key))
+			# decrypt file 
+			plain_file = decrypt_file(encr_file_path=myargs["-d"] + "/" + metadata[f]["uuid"], key=fernet_key)
+
+			# write decrypted file to disk
+			write_file(file_name=out_folder + f, data=plain_file)
 			
-			# show stats
+			## Stats
 			decr_files_ctr += 1
+
+			# show stats
 			if not g_VERBOSE:				
 				if decr_files_ctr % 10 == 0:
 					print("decrypted " + str(decr_files_ctr) + "/" + str(len(metadata)) + " so far")
